7a011659cd2e03a4c632ad21f625a9bbabb72272
rajathsm <nagarajathsm@gmail.com>
2020-11-22 21:58:03 +0530

    user input

---
+++ Input.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Input{
+
+public static void main(String args[]){
+
+//type identifier=new type();
+
+Scanner scanner=new Scanner(System.in);
+
+String name=scanner.nextLine();
+//String name="input file";
+
+System.out.println(name);
+
+}
+
+}
