82f3fb7f374e89365d9bca5407fab872531516d8
rajathsm <nagarajathsm@gmail.com>
2020-11-22 21:46:42 +0530

    hello world

---
+++ Hello.java
@@ -0,0 +1,16 @@
+
+
+public class Hello{
+
+//static  - no instance of class is needed
+
+
+public static void main(String args[]){
+
+System.out.println("hello");
+
+System.out.print("args"+args[0]); //cmd arguments
+
+}
+
+}
