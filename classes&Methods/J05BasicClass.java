5a79d1cb7556e462b066d4d4acd570cd206ae53c
rajathsm <nagarajathsm@gmail.com>
2020-11-29 02:24:57 +0530

    Encapsulation and static methods

--- classes&Methods/J05BasicClass.java
+++ classes&Methods/J05BasicClass.java
@@ -1,3 +1,6 @@
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
@@ -5,12 +8,30 @@ public class J05BasicClass {
 
 public static void main(String args[]){
 
+        PrintStream p = System.out;
+
         User user=new User();
         user.name="java";
         System.out.println(user.name);
 
         String rep=user.repeatStr(50);
         System.out.println(rep);
+
+        User user2=new User();
+        user2.setAge(10);
+
+        p.println(user2.getAge());
+
+
+
+        //custom arraylist type
+        List<User> numOfUsers=new ArrayList<User>();
+        numOfUsers.add(user);
+        numOfUsers.add(user2);
+
+        //invoking static User method
+        User.statMethod(user2);
+
 
 
 }
--- classes&Methods/User.java
+++ classes&Methods/User.java
@@ -4,10 +4,26 @@ public class User{
 
 
         public String name;
+        private int age;
 
         public String repeatStr(int num){
             String h="hello";
             return h.repeat(num);
+        }
+
+        //getter
+        public int getAge(){
+            return age;
+        }
+
+        //setter
+        public void setAge(int ag){
+            age=ag;
+        }
+
+        //static method
+        public static void statMethod(User u){
+            System.out.println(u.getAge());
         }
 
 }
